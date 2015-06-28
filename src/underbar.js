580848bc63ee4bacf42053db997e0340a1124953
Aaron Wolfe <aar.wolfe@gmail.com>
2015-06-28 02:35:50 -0400

    Completed the functions:  every, some, extend, and defaults

--- src/underbar.js
+++ src/underbar.js
@@ -211,13 +211,41 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    
+    if(collection.length <= 0) {
+      return true;
+    }
+    if(iterator === undefined) {
+      iterator = Boolean;
+    }
+    
+    return _.reduce(collection, function(pass, item){
+      if(!pass) {
+        return false;
+      }
+      return true === Boolean(iterator(item));
+    }, true);
+    
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
-  };
+    if(collection.length === 0) {
+      return false;
+    }
+    if(iterator === undefined) {
+      iterator = Boolean;
+    }
+
+    var result = _.map(collection, function(x) {
+      return Boolean(iterator(x));
+    });
+
+    return _.contains(result, true);
+
+    };
 
 
   /**
@@ -239,11 +267,33 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var temp;
+
+    for(var i = 1; i < arguments.length; i ++) {
+      temp = arguments[i];
+      for(var prop in temp) {
+        obj[prop] = temp[prop];
+      }
+    }
+
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var temp;
+
+    for(var i = 1; i < arguments.length; i ++) {
+      temp = arguments[i];
+      for(var prop in temp) {
+        if(obj[prop] === undefined) {
+          obj[prop] = temp[prop];
+        }
+      }
+    }
+
+    return obj;
   };
 
 
