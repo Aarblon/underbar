a5746eebf5ec387dbde889558865b2879cb11dfc
Aaron Wolfe <aar.wolfe@gmail.com>
2015-07-06 15:41:55 -0400

    Removed the incomplete version of invoke.

--- src/underbar.js
+++ src/underbar.js
@@ -412,7 +412,7 @@
   // Calls the method named by functionOrKey on each value in the list.
   // Note: You will need to learn a bit about .apply to complete this.
   _.invoke = function(collection, functionOrKey, args) {
-    return collection.apply(functionOrKey, args);
+    
     
   };
 
